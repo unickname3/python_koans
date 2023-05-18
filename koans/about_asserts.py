876e188f3834e12b046d6454c85ed3a233779e89
Igor Dikarev <igor.dikarev@gmail.com>
2023-05-18 20:59:50 +0300

    solve few tasks

--- koans/about_asserts.py
+++ koans/about_asserts.py
@@ -3,8 +3,8 @@
 
 from runner.koan import *
 
-class AboutAsserts(Koan):
 
+class AboutAsserts(Koan):
     def test_assert_truth(self):
         """
         We shall contemplate truth by testing reality, via asserts.
@@ -14,13 +14,13 @@ class AboutAsserts(Koan):
         #
         #   http://bit.ly/about_asserts
 
-        self.assertTrue(False) # This should be True
+        self.assertTrue(True)  # This should be True
 
     def test_assert_with_message(self):
         """
         Enlightenment may be more easily achieved with appropriate messages.
         """
-        self.assertTrue(False, "This should be True -- Please fix this")
+        self.assertTrue(True, "This should be True -- Please fix this")
 
     def test_fill_in_values(self):
         """
@@ -70,9 +70,8 @@ class AboutAsserts(Koan):
         #
         # See for yourself:
 
-        self.assertEqual(__, "navel".__class__) # It's str, not <type 'str'>
+        self.assertEqual(__, "navel".__class__)  # It's str, not <type 'str'>
 
         # Need an illustration? More reading can be found here:
         #
         #   https://github.com/gregmalcolm/python_koans/wiki/Class-Attribute
-
