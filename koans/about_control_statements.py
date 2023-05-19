9d1982f587e73af6d7b4b5a404b5203b87b307f3
Igor Dikarev <igor.dikarev@gmail.com>
2023-05-19 17:14:53 +0300

    solve control statement

--- koans/about_control_statements.py
+++ koans/about_control_statements.py
@@ -3,29 +3,29 @@
 
 from runner.koan import *
 
-class AboutControlStatements(Koan):
 
+class AboutControlStatements(Koan):
     def test_if_then_else_statements(self):
         if True:
-            result = 'true value'
+            result = "true value"
         else:
-            result = 'false value'
-        self.assertEqual(__, result)
+            result = "false value"
+        self.assertEqual("true value", result)
 
     def test_if_then_statements(self):
-        result = 'default value'
+        result = "default value"
         if True:
-            result = 'true value'
-        self.assertEqual(__, result)
+            result = "true value"
+        self.assertEqual("true value", result)
 
     def test_if_then_elif_else_statements(self):
         if False:
-            result = 'first value'
+            result = "first value"
         elif True:
-            result = 'true value'
+            result = "true value"
         else:
-            result = 'default value'
-        self.assertEqual(__, result)
+            result = "default value"
+        self.assertEqual("true value", result)
 
     def test_while_statement(self):
         i = 1
@@ -33,45 +33,47 @@ class AboutControlStatements(Koan):
         while i <= 10:
             result = result * i
             i += 1
-        self.assertEqual(__, result)
+        self.assertEqual(3628800, result)
 
     def test_break_statement(self):
         i = 1
         result = 1
         while True:
-            if i > 10: break
+            if i > 10:
+                break
             result = result * i
             i += 1
-        self.assertEqual(__, result)
+        self.assertEqual(3628800, result)
 
     def test_continue_statement(self):
         i = 0
         result = []
         while i < 10:
             i += 1
-            if (i % 2) == 0: continue
+            if (i % 2) == 0:
+                continue
             result.append(i)
-        self.assertEqual(__, result)
+        self.assertEqual([1, 3, 5, 7, 9], result)
 
     def test_for_statement(self):
         phrase = ["fish", "and", "chips"]
         result = []
         for item in phrase:
             result.append(item.upper())
-        self.assertEqual([__, __, __], result)
+        self.assertEqual(["FISH", "AND", "CHIPS"], result)
 
     def test_for_statement_with_tuples(self):
         round_table = [
             ("Lancelot", "Blue"),
             ("Galahad", "I don't know!"),
             ("Robin", "Blue! I mean Green!"),
-            ("Arthur", "Is that an African Swallow or European Swallow?")
+            ("Arthur", "Is that an African Swallow or European Swallow?"),
         ]
         result = []
         for knight, answer in round_table:
             result.append("Contestant: '" + knight + "'   Answer: '" + answer + "'")
 
-        text = __
+        text = "Robin"
 
         self.assertRegex(result[2], text)
 
