16b1f6b672e1cb5e0e9936b555c2d92ee639589a
Igor Dikarev <igor.dikarev@gmail.com>
2023-05-25 22:06:09 +0300

    solve monkey patching

--- koans/about_monkey_patching.py
+++ koans/about_monkey_patching.py
@@ -7,6 +7,7 @@
 
 from runner.koan import *
 
+
 class AboutMonkeyPatching(Koan):
     class Dog:
         def bark(self):
@@ -14,18 +15,20 @@ class AboutMonkeyPatching(Koan):
 
     def test_as_defined_dogs_do_bark(self):
         fido = self.Dog()
-        self.assertEqual(__, fido.bark())
+        self.assertEqual("WOOF", fido.bark())
 
     # ------------------------------------------------------------------
 
     # Add a new method to an existing class.
     def test_after_patching_dogs_can_both_wag_and_bark(self):
-        def wag(self): return "HAPPY"
+        def wag(self):
+            return "HAPPY"
+
         self.Dog.wag = wag
 
         fido = self.Dog()
-        self.assertEqual(__, fido.wag())
-        self.assertEqual(__, fido.bark())
+        self.assertEqual("HAPPY", fido.wag())
+        self.assertEqual("WOOF", fido.bark())
 
     # ------------------------------------------------------------------
 
@@ -35,14 +38,15 @@ class AboutMonkeyPatching(Koan):
         except Exception as ex:
             err_msg = ex.args[0]
 
-        self.assertRegex(err_msg, __)
+        self.assertRegex(err_msg, "immutable")
 
     # ------------------------------------------------------------------
 
-    class MyInt(int): pass
+    class MyInt(int):
+        pass
 
     def test_subclasses_of_built_in_classes_can_be_be_monkey_patched(self):
         self.MyInt.is_even = lambda self: (self % 2) == 0
 
-        self.assertEqual(__, self.MyInt(1).is_even())
-        self.assertEqual(__, self.MyInt(2).is_even())
+        self.assertEqual(False, self.MyInt(1).is_even())
+        self.assertEqual(True, self.MyInt(2).is_even())
